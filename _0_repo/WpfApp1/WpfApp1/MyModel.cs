5c9a43148899703041a7ea6fdafadf4a3cb1a719
ksj10111011@naver.com <ksj10111011@naver.com>
2024-12-19 11:44:26 +0900

    feat : community toolkit 적용 완료 Datacontext에 viewmodel을 key로 받아야 처리가 가능하다.(toolkit에서 partial로 class가 나누어져서 그런듯) relaycommand, ObservableProperty, ObservableCollection 테스트 완료

---
+++ _0_repo/WpfApp1/WpfApp1/MyModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public int Count {  get; set; }
+
+        public MyModel(int id)
+        {
+            Id = id;
+            Name = string.Empty;
+            Description = string.Empty;
+            Type = string.Empty;
+        }
+    }
+}
---
+++ _0_repo/WpfApp1/WpfApp1/MyViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+
+namespace WpfApp1
+{
+    public partial class MyViewModel : ObservableObject
+    {
+        MyModel model_inst;
+
+        public MyViewModel()
+        {
+            model_inst = new MyModel(id: 0);
+            Vm_name = "12345";
+            Count = 0;
+            Collection_test = new ObservableCollection<string>() { Count.ToString() };
+        }
+
+        [ObservableProperty]
+        private int vm_id;
+
+        [ObservableProperty]
+        private string vm_name;
+
+        [ObservableProperty]
+        private string vm_type;
+
+        [ObservableProperty]
+        private string vm_description;
+
+        [ObservableProperty]
+        private ObservableCollection<string> collection_test;
+
+        [ObservableProperty]
+        private int count;
+
+        [RelayCommand]
+        private void viewModelCommandTest()
+        {
+            Vm_name = "Clicked " + Collection_test[Count].ToString();
+            Count += 1;
+            Collection_test.Add(Count.ToString());
+        }
+    }
+}
