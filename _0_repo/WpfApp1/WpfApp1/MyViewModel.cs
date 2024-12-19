caae83a8fcf7f115cc95dc1b0ac3118a9d64e2c5
ksj10111011@naver.com <ksj10111011@naver.com>
2024-12-19 13:01:20 +0900

    feat : sql 처리

--- _0_repo/WpfApp1/WpfApp1/MyViewModel.cs
+++ _0_repo/WpfApp1/WpfApp1/MyViewModel.cs
@@ -13,14 +13,23 @@ namespace WpfApp1
     public partial class MyViewModel : ObservableObject
     {
         MyModel model_inst;
+        readonly UserServiceDB user_service_inst;
 
         public MyViewModel()
         {
+            user_service_inst = new UserServiceDB();
+
             model_inst = new MyModel(id: 0);
             Vm_name = "12345";
             Count = 0;
             Collection_test = new ObservableCollection<string>() { Count.ToString() };
+
+            User_collect = new ObservableCollection<User>();
+           
         }
+
+        [ObservableProperty]
+        private ObservableCollection<User> user_collect;
 
         [ObservableProperty]
         private int vm_id;
@@ -46,6 +55,24 @@ namespace WpfApp1
             Vm_name = "Clicked " + Collection_test[Count].ToString();
             Count += 1;
             Collection_test.Add(Count.ToString());
+        }
+
+        [RelayCommand]
+        private void AddUser()
+        {
+            user_service_inst.AddUser("John Doe", 30); // Dummy user
+            LoadUsers(); // Refresh user list
+        }
+
+        [RelayCommand]
+        private void LoadUsers()
+        {
+            User_collect.Clear();
+            var users = user_service_inst.GetUsers();
+            foreach (var user in users)
+            {
+                User_collect.Add(user);
+            }
         }
     }
 }
---
+++ _0_repo/WpfApp1/WpfApp1/UserServiceDB.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+    public class UserServiceDB
+    {
+        private const string DbFile = "Data Source=users.db";
+
+        public UserServiceDB()
+        {
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            try
+            {
+
+                using (var connection = new SQLiteConnection(DbFile))
+                {
+                    connection.Open();
+                    string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Users (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL,
+                        Age INTEGER NOT NULL
+                    )";
+                    SQLiteCommand command = new SQLiteCommand(createTableQuery, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void AddUser(string name, int age)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(DbFile))
+                {
+                    connection.Open();
+                    string insertQuery = "INSERT INTO Users (Name, Age) VALUES (@Name, @Age)";
+                    SQLiteCommand command = new SQLiteCommand(insertQuery, connection);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Age", age);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            using (var connection = new SQLiteConnection(DbFile))
+            {
+                connection.Open();
+                string selectQuery = "SELECT * FROM Users";
+                SQLiteCommand command = new SQLiteCommand(selectQuery, connection);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        users.Add(new User
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Age = reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+            return users;
+        }
+    }
+}
